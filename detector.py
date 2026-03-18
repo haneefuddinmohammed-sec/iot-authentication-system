dab30e55c26ea88ad616e90a465091b70128ebb3
haneefuddinmohammed-sec <haneefuddinmohammad@gmail.com>
2026-03-18 13:23:50 +0000

    Update detector.py

--- detector.py
+++ detector.py
@@ -7,4 +7,8 @@ def detect_threat(device_id):
     failed_attempts[device_id] += 1
 
     if failed_attempts[device_id] >= 3:
-        print(f"[ALERT] Suspicious activity from {device_id}")
+        alert_msg = f"[ALERT] Suspicious activity from {device_id}"
+        print(alert_msg)
+
+        with open("alerts.log", "a") as file:
+            file.write(alert_msg + "\n")
